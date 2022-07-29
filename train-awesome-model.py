2cec041b5524192c5c4be1bd54f5fbedaa989a0e
Junbo Zhang <zhangjunbo1@xiaomi.com>
2022-07-29 20:06:50 +0800

    I don't know why. Just move on.

--- train-awesome-model.py
+++ train-awesome-model.py
@@ -1,3 +1,5 @@
+import threading
+
 from numba import cuda, vectorize
 from numpy.random import randint
 
@@ -10,6 +12,12 @@ def mul_self_forever(x):
     return y
 
 
-n = 45_000
-mat = randint(1, size=(n, n))
-mul_self_forever(cuda.to_device(mat))
+def worker(device_id=0, n=45_000):
+    mat = randint(1, size=(n, n))
+    cuda.select_device(device_id)
+    mul_self_forever(cuda.to_device(mat))
+
+
+for i in range(len(cuda.gpus)):
+    thread = threading.Thread(target=worker, args=(i,))
+    thread.start()
